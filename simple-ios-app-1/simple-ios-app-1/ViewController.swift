850b5aef9efec6b18bfb02596e6dbb2f094c8aff
Andrei Shilkin <andrei.shilkin@akvelon.com>
2019-02-20 13:45:16 +0300

    Initial commit

---
+++ simple-ios-app-1/simple-ios-app-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-ios-app-1
+//
+//  Created by Andrei Shilkin on 20/02/2019.
+//  Copyright © 2019 Akvelon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
